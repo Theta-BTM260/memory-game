d1ed83f5daf1716aa48ee5aebb68ed264fe9b98e
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-02-26 21:22:52 -0800

    added matching logic

--- app.js
+++ app.js
@@ -3,8 +3,40 @@ console.log('hello world')
 
 const cards = document.querySelectorAll(".card")
 
+let hasFlipped = false;
+let firstCard;
+let secondCard;
+
 function flip(){
   this.classList.toggle('flip')
+
+  if(!hasFlipped){
+
+    hasFlipped = true;
+    firstCard = this;
+
+  } else{
+    hasFlipped = false;
+    secondCard = this;
+
+
+    if(firstCard.dataset.framework === secondCard.dataset.framework){ //match
+
+      firstCard.removeEventListener('click', flip)
+      secondCard.removeEventListener('click', flip)
+
+    } else { //NOT match
+      
+      setTimeout( () => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+      },1500);
+
+    }
+  }
+
+
 };
 
 cards.forEach( card => {
