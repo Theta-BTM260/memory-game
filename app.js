6d4e70148f6ff16ef34d7e973b8c747d4e6c827f
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-02-26 20:57:07 -0800

    working on js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict'
 console.log('hello world')
 
+const cards = document.querySelectorAll(".card")
+
+function flip(){
+  
+};
+
+cards.forEach( card => {
+  card.addEventListener('click', flip)
+});
